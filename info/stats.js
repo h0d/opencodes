28f4f7ab6d84af1ac9f7f658d4138abf7eefdf73
rho2 <rho2bin@gmail.com>
2018-03-24 23:06:35 +0100

    Added the basic structure for the statistics page

---
+++ info/stats.js
@@ -0,0 +1,47 @@
+var test = [
+    {
+        "id" : "loc",
+        "text" : "removed locations",
+        "value" : 0
+    },
+    {
+        "id" : "cam",
+        "text" : "removed camera info",
+        "value" : 0
+    },
+    {
+        "id" : "det",
+        "text" : "detections",
+        "value" : 0
+    }
+]
+
+window.onload = function(){
+    var c = document.getElementById("content")
+    this.console.log("Test")
+
+    for(i in test){
+        var e = test[i];
+        var div = document.createElement("div");
+        
+        div.classList.add("pure-u-lg-1-3");
+        div.classList.add("pure-u-sm-1-2");
+        div.classList.add("info-grid");
+
+        var header = document.createElement("p");
+        var info = document.createElement("p");
+
+        header.classList.add("info-hd");
+        info.classList.add("info");
+
+        header.innerHTML = e.text;
+        info.innerHTML = e.value;
+
+        div.appendChild(header);
+        div.appendChild(info);
+
+        c.appendChild(div);
+
+        console.log(e);
+    }
+};

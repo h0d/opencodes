2b9b728a7d74a5e62cc796a4446328b0999cafaa
rho2 <rho2bin@gmail.com>
2018-03-25 06:04:30 +0200

    did some stuff which hopefully works

--- info/stats.js
+++ info/stats.js
@@ -20,28 +20,36 @@ window.onload = function(){
     var c = document.getElementById("content")
     this.console.log("Test")
 
-    for(i in test){
-        var e = test[i];
-        var div = document.createElement("div");
+    chrome.storage.sync.get(['count'], function (result) {
+        console.log(result)
+
+            for(i in test){
+                var e = test[i];
+                var div = document.createElement("div");
+                
+                div.classList.add("pure-u-lg-1-3");
+                div.classList.add("pure-u-sm-1-2");
+                div.classList.add("info-grid");
         
-        div.classList.add("pure-u-lg-1-3");
-        div.classList.add("pure-u-sm-1-2");
-        div.classList.add("info-grid");
+                var header = document.createElement("p");
+                var info = document.createElement("p");
+        
+                header.classList.add("info-hd");
+                info.classList.add("info");
+        
+                header.innerHTML = e.text;
+                info.innerHTML = e.value;
+        
+                div.appendChild(header);
+                div.appendChild(info);
+        
+                c.appendChild(div);
+        
+                console.log(e);
+            }
 
-        var header = document.createElement("p");
-        var info = document.createElement("p");
-
-        header.classList.add("info-hd");
-        info.classList.add("info");
-
-        header.innerHTML = e.text;
-        info.innerHTML = e.value;
-
-        div.appendChild(header);
-        div.appendChild(info);
-
-        c.appendChild(div);
-
-        console.log(e);
-    }
+            chrome.storage.sync.set({'count': test}, function() {
+                console.log('Value is set to ' + test);
+              });
+    });
 };
